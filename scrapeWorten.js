b3c545afc24d8daa7f135ea5542cda7f8b08155f
zedosoad <joao.pires.queiroz@gmail.com>
2021-10-31 19:49:18 +0100

    Get details of product

--- scrapeWorten.js
+++ scrapeWorten.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+const cheerio = require("cheerio");
 
 async function getEsquentadores(){
 
@@ -15,32 +15,47 @@ async function getEsquentadores(){
 
     while(!lastPage){
 
+        var model;
+
         await axios.get("https://www.worten.pt/grandes-eletrodomesticos/aquecimento-de-agua/esquentadores?x-event-type=product_list%3Arefresh&page=" + pageNum, axiosConfig).then(resp => {
 
             for(let module of Object.values(resp.data["modules"])){
                 if(module['model']['template'] == 'product_list'){
-                    var model = module['model'];
-                    //console.log(model);
+                    model = module['model'];
                 }
             }
 
-            for(let product of model['products']){
-                console.log("name:", product['name']);
-                console.log("brand:", product['brand']);
-                console.log("color:", product['color']);
-                console.log("energy-class:", product['energy-class']);
-                console.log("description:", product['description']);
-                console.log("price:", product['price']);
-                console.log("rating:", product['rating_bazaar']);
-                console.log("num reviews:", product['reviews_bazaar']);
-                console.log();
-            }
-
-            if(model['offset']['max'] >= model['offsetMax']){
-                lastPage = true;
-            }
-
         });
+
+        for(let product of model['products']){
+            console.log("name:", product['name']);
+            console.log("brand:", product['brand']);
+            console.log("color:", product['color']);
+            console.log("energy-class:", product['energy-class']);
+            console.log("description:", product['description']);
+            console.log("price:", product['price']);
+            console.log("rating:", product['rating_bazaar']);
+            console.log("num reviews:", product['reviews_bazaar']);
+            console.log("url:", product['default_url']);
+
+            await axios.get("https://www.worten.pt" + product['default_url']).then(resp => {
+                //console.log(resp.data);
+                const $ = cheerio.load(resp.data);
+
+                $("li[class='clearfix']").each(function (i, e) {
+                    console.log($(e).find('.details-label').contents().last().text() + ":",
+                                $(e).find('.details-value').text());
+                });
+
+            })
+
+            console.log();
+
+        }
+
+        if(model['offset']['max'] >= model['offsetMax']){
+            lastPage = true;
+        }
 
         pageNum++;
 
