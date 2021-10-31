1308901bd701bcb4c2d0658e72cbf6528fbd0ec8
zedosoad <joao.pires.queiroz@gmail.com>
2021-10-31 23:39:37 +0100

    saves product info to dictionary allows multiple products

--- scrapeWorten.js
+++ scrapeWorten.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 const cheerio = require("cheerio");
 
-async function getEsquentadores(){
+var scrapedProducts = [];
+
+async function getProdutosTipo(url, details = false){
 
     const axiosConfig = {
         headers: {
@@ -17,7 +19,7 @@ async function getEsquentadores(){
 
         var model;
 
-        await axios.get("https://www.worten.pt/grandes-eletrodomesticos/aquecimento-de-agua/esquentadores?x-event-type=product_list%3Arefresh&page=" + pageNum, axiosConfig).then(resp => {
+        await axios.get(url + "?x-event-type=product_list%3Arefresh&page=" + pageNum, axiosConfig).then(resp => {
 
             for(let module of Object.values(resp.data["modules"])){
                 if(module['model']['template'] == 'product_list'){
@@ -28,7 +30,20 @@ async function getEsquentadores(){
         });
 
         for(let product of model['products']){
-            console.log("name:", product['name']);
+            
+            product_obj = {};
+            product_obj['name'] = product['name'];
+            product_obj['brand'] = product['brand'];
+            product_obj['color'] = product['color'];
+            product_obj['energy-class'] = product['energy-class'];
+            product_obj['description'] = product['description'];
+            product_obj['price'] = product['price'];
+            product_obj['rating'] = product['rating_bazaar'];
+            product_obj['num-reviews'] = product['reviews_bazaar'];
+            product_obj['categories'] = product['category_path'];
+            product_obj['url'] = product['default_url'];
+
+            /*console.log("name:", product['name']);
             console.log("brand:", product['brand']);
             console.log("color:", product['color']);
             console.log("energy-class:", product['energy-class']);
@@ -36,20 +51,24 @@ async function getEsquentadores(){
             console.log("price:", product['price']);
             console.log("rating:", product['rating_bazaar']);
             console.log("num reviews:", product['reviews_bazaar']);
-            console.log("url:", product['default_url']);
+            console.log("categories:", product['category_path']);
+            console.log("url:", product['default_url']);*/
 
-            await axios.get("https://www.worten.pt" + product['default_url']).then(resp => {
-                //console.log(resp.data);
-                const $ = cheerio.load(resp.data);
+            if(details){
+                await axios.get("https://www.worten.pt" + product['default_url']).then(resp => {
+                    const $ = cheerio.load(resp.data);
 
-                $("li[class='clearfix']").each(function (i, e) {
-                    console.log($(e).find('.details-label').contents().last().text() + ":",
-                                $(e).find('.details-value').text());
-                });
+                    $("li[class='clearfix']").each(function (i, e) {
+                        //console.log($(e).find('.details-label').contents().last().text() + ":",
+                        //            $(e).find('.details-value').text());
+                        product_obj[$(e).find('.details-label').contents().last().text()] = $(e).find('.details-value').text();
+                    });
 
-            })
+                })
+                //console.log();
+            }
 
-            console.log();
+            scrapedProducts.push(product_obj);
 
         }
 
@@ -62,4 +81,16 @@ async function getEsquentadores(){
     }
 }
 
-getEsquentadores();
+const listUrls = ["https://www.worten.pt/grandes-eletrodomesticos/aquecimento-de-agua/esquentadores",
+                    "https://www.worten.pt/grandes-eletrodomesticos/aquecimento-de-agua/termoacumuladores"];
+
+num_started_scrapes = 0;
+for(let url of listUrls){
+    num_started_scrapes++;
+    getProdutosTipo(url, true).then(() => {
+        num_started_scrapes--;
+        if(num_started_scrapes == 0){
+            console.log(scrapedProducts.length, scrapedProducts[0]);
+        }
+    });
+}
