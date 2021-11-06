fb9ad76c1860d86e2c2ddbefd81de5e191fce0de
zedosoad <joao.pires.queiroz@gmail.com>
2021-11-06 14:36:14 +0100

    Solved existing product storage bug, Solved Type conversion exception

--- scrapeWorten.js
+++ scrapeWorten.js
@@ -5,9 +5,7 @@ const Promise = require("bluebird");
 
 
 var numbersKeys = ['Capacidade (L/min)', 'Pressão (bar)', 'Potência (W)', 'Peso', 'Altura', 'Largura', 'Profundidade', 'Temperatura Mínima (ºC)',
-                    'Caudal Min.', 'Caudal Máx.', 'Potência (kW)', 'Pressão máxima (bar)', 'Tubo de Exaustão (mm)', 'Consumo de Gás (Gj/annum)'];
-
-var numbersRemoveEndKeys = {'Temperatura Máxima (ºC)': ' ºC'};
+                    'Caudal Min.', 'Caudal Máx.', 'Potência (kW)', 'Pressão máxima (bar)', 'Tubo de Exaustão (mm)', 'Consumo de Gás (Gj/annum)', 'Temperatura Máxima (ºC)'];
 
 var boolKeys = ['Regulador de temperatura', 'Válvula segurança', 'Válvula de descarga', 'Ecrã Digital',
                 'Regulação débito gás', 'Regulação débito água', 'Comp.paineis solares', 'Interruptor on/off'];
@@ -18,15 +16,7 @@ async function getProductInfo(product, details){
     var product_obj = {};
     var exists = false;
 
-    var existingProducts = scrapedProducts.filter(function (el) {
-        return el.url == product['default_url'];
-    });
-    if(existingProducts.length > 0){
-        exists = true;
-        //var indexProduct = scrapedProducts.indexOf(existingProduct[0]);
-        var existingProduct = existingProducts[0];
-    }
-
+    // Get basic info
     product_obj['name'] = product['name'];
     product_obj['brand'] = product['brand'];
     product_obj['color'] = product['color'];
@@ -37,15 +27,23 @@ async function getProductInfo(product, details){
     product_obj['categories'] = product['category_path'][product['category_path'].length-1];
     product_obj['url'] = product['default_url'];
 
+    var productIdx = scrapedProducts.findIndex(el =>el.url == product['default_url']);
+    if(productIdx != -1){
+        exists = true;
+    }
+
     var today = new Date();
     var todayStr = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     if(exists){
+        var existingProduct = scrapedProducts[productIdx];
+        product_obj['prices'] = existingProduct['prices'];
+
         let lastPrice = existingProduct['prices'][existingProduct['prices'].length-1];
         if(product['price'] != lastPrice['value']){
             if(todayStr == lastPrice['date']){
-                lastPrice = product['price'];
+                product_obj['prices'][product_obj['prices'].length-1] = product['price'];
             }else{
-                existingProduct['prices'].push({'date': todayStr, 'value': product['price']});
+                product_obj['prices'].push({'date': todayStr, 'value': product['price']});
             }
         }
     }else{
@@ -54,7 +52,8 @@ async function getProductInfo(product, details){
 
     console.log(product_obj['url']);
 
-    if(details && !exists){
+    // Get more details of product
+    if(true){//details && !exists){
 
         await axios.get("https://www.worten.pt" + product['default_url']).then(resp => {
             const $ = cheerio.load(resp.data);
@@ -64,29 +63,30 @@ async function getProductInfo(product, details){
             $("li[class='clearfix']").each(function (i, e) {
                 let key = $(e).find('.details-label').contents().last().text();
                 product_obj['more-details'][key] = $(e).find('.details-value').text();
+                // Funcao para conversao de tipo
                 if(numbersKeys.includes(key)){
-                    product_obj['more-details'][key] = Number(product_obj['more-details'][key]);
-                }
-                else if(key in numbersRemoveEndKeys){
-                    product_obj['more-details'][key] = Number(product_obj['more-details'][key].substring(0, numbersRemoveEndKeys[key].length));
+                    let convValue = Number(product_obj['more-details'][key].split(' ')[0].replace(",", ".").replace(/[A-Za-z]/g, ''));
+                    if(!isNaN(convValue)){
+                        product_obj['more-details'][key] = convValue;
+                    }
                 }
                 else if(boolKeys.includes(key)){
                     product_obj['more-details'][key] = (product_obj['more-details'][key] == "Sim") ? true : false;
                 }
-            });
+            })
 
         }).catch(function (error) {
             console.log(error);
         });
-        //console.log();
     }
 
     if(exists){
-        existingProduct = product_obj;
+        scrapedProducts[productIdx] = product_obj;
     }else{
         scrapedProducts.push(product_obj);
     }
 }
+
 
 async function getProdutosTipo(url, details = false){
 
@@ -107,9 +107,11 @@ async function getProdutosTipo(url, details = false){
         var success = true;
         await axios.get(url + "?x-event-type=product_list%3Arefresh&page=" + pageNum, axiosConfig).then(resp => {
 
+            // Find JSON with products
             for(let module of Object.values(resp.data["modules"])){
                 if(module['model']['template'] == 'product_list'){
                     model = module['model'];
+                    break;
                 }
             }
 
@@ -117,7 +119,7 @@ async function getProdutosTipo(url, details = false){
             console.log(error);
             success = false;
         });
-        if(!success){
+        if(!success || model === undefined){
             return;
         }
 
@@ -126,13 +128,14 @@ async function getProdutosTipo(url, details = false){
             return;
         }
 
+        // get product details
         await Promise.map(model['products'],
             (product) => getProductInfo(product, details),
             { concurrency: 3 }
         ).catch((error) => console.log(error));
 
 
-        if(!('max' in model['offset'] || 'offsetMax' in model)){
+        if(!('max' in model['offset'] && 'offsetMax' in model)){
             console.log("ERROR: No Key 'offset' or 'offsetMax'");
             return;
         }
@@ -145,6 +148,7 @@ async function getProdutosTipo(url, details = false){
     }
 }
 
+// Helper functions
 const loadData = (path) => {
     try {
         return JSON.parse(fs.readFileSync(path, 'utf8'));
@@ -161,12 +165,15 @@ const storeData = (data, path) => {
     }
 }
 
+// Lista numa pasta
 const listUrls = ["https://www.worten.pt/grandes-eletrodomesticos/aquecimento-de-agua/esquentadores",
                     "https://www.worten.pt/grandes-eletrodomesticos/aquecimento-de-agua/termoacumuladores"];
 
 
 var scrapedProducts = loadData("resources/wortenData.json");
 
+// Function que faz scraping e faz update da variavel.
+// Depois gravar e error handling
 Promise.map(listUrls,
     url => getProdutosTipo(url, true),
     { concurrency: 2 }
